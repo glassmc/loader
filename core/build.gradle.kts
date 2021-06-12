48c0ffd6f8ebe7f5b1400a2561dfcf52e90e2c47
danterusdev <62973372+danterusdev@users.noreply.github.com>
2021-06-11 17:59:23 -0700

    removed unnecessary stuff

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm") version "1.5.10"
-    id("maven-publish")
 }
 
 repositories {
@@ -13,18 +12,4 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
-}
-
-
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "ml.glassmc"
-            artifactId = "loader"
-            version = "0.0.1"
-
-            from(components["java"])
-        }
-    }
 }
