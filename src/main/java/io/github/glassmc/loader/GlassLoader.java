bb32dc31bb519f38ecb5c3feafdec8c2254126b8
danterusdev <danterusdev@gmail.com>
2021-07-05 16:20:22 -0700

    bump version

--- src/main/java/io/github/glassmc/loader/GlassLoader.java
+++ src/main/java/io/github/glassmc/loader/GlassLoader.java
@@ -33,7 +33,7 @@ public class GlassLoader {
     private final Map<Class<?>, Object> interfaces = new HashMap<>();
 
     private GlassLoader() {
-        this.registerVirtualShard(new ShardSpecification("loader", "0.1.0"));
+        this.registerVirtualShard(new ShardSpecification("loader", "0.2.0"));
     }
 
     public void appendExternalShards() {
