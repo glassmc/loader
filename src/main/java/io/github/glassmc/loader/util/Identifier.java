4f0027fb5aa45b28eca09f65a4b510dc69538bf4
danterusdev <danterusdev@gmail.com>
2021-07-05 14:43:54 -0700

    added identifier - an easier way to parse name strings

---
+++ src/main/java/io/github/glassmc/loader/util/Identifier.java
@@ -0,0 +1,60 @@
+package io.github.glassmc.loader.util;
+
+public class Identifier {
+
+    private final String className;
+
+    private final String fieldName;
+
+    private final String methodName;
+    private final String methodDesc;
+
+    public static Identifier parse(String identifier) {
+        String className;
+        String fieldName = null;
+        String methodName = null;
+        String methodDesc = null;
+
+        if(identifier.contains("#")) {
+            String[] classElementSplit = identifier.split("#");
+            className = classElementSplit[0];
+            if(classElementSplit[1].contains("(")) {
+                String[] methodNameDescSplit = classElementSplit[1].split("\\(");
+                methodNameDescSplit[1] = "(" + methodNameDescSplit[1];
+
+                methodName = methodNameDescSplit[0];
+                methodDesc = methodNameDescSplit[1];
+            } else {
+                fieldName = classElementSplit[1];
+            }
+        } else {
+            className = identifier;
+        }
+
+        return new Identifier(className, fieldName, methodName, methodDesc);
+    }
+
+    public Identifier(String className, String fieldName, String methodName, String methodDesc) {
+        this.className = className;
+        this.fieldName = fieldName;
+        this.methodName = methodName;
+        this.methodDesc = methodDesc;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public String getMethodDesc() {
+        return methodDesc;
+    }
+
+}
