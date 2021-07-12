09b04e1571c6e8c90716dd6704f26b24c3f1cdac
danterusdev <danterusdev@gmail.com>
2021-07-12 15:33:23 -0700

    bump version (even though actualy 0.3.1 release was 5 days ago)

--- src/main/java/com/github/glassmc/loader/GlassLoader.java
+++ src/main/java/com/github/glassmc/loader/GlassLoader.java
@@ -35,7 +35,7 @@ public class GlassLoader {
     private final Map<Class<?>, Object> interfaces = new HashMap<>();
 
     private GlassLoader() {
-        this.registerVirtualShard(new ShardSpecification("loader", "0.3.0"));
+        this.registerVirtualShard(new ShardSpecification("loader", "0.3.1"));
     }
 
     public void appendExternalShards() {
