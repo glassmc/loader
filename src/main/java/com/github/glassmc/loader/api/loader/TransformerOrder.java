13be6b4a244b4381a96cdcd70d92f061bc06be73
danterusdev <danterusdev@gmail.com>
2022-04-23 05:48:43 -0700

    Fix typo

--- src/main/java/com/github/glassmc/loader/api/loader/TransformerOrder.java
+++ src/main/java/com/github/glassmc/loader/api/loader/TransformerOrder.java
@@ -1,7 +1,7 @@
 package com.github.glassmc.loader.api.loader;
 
 /**
- * Allows for a shard to request a specific ordering for a transformer, determining if it is run before or after shards also transforming a specific Glass.
+ * Allows for a shard to request a specific ordering for a transformer, determining if it is run before or after shards also transforming a specific class.
  */
 public enum TransformerOrder {
     FIRST,
